245a1c844a50b6071390b329c350e714f92b5c90
Deeksha Rajak <158442937+deeksha0606@users.noreply.github.com>
2024-04-25 13:26:44 +0530

    added this website

---
+++ caly/index.js
@@ -0,0 +1,22 @@
+// const Button = document.querySelector('.nav-toggel')
+// const nav=document.querySelector('.navigation-bar ul')
+// console.log(Button)
+// console.log(nav)
+
+// Button.addEventListener('click', ()=> {
+//     nav.classList.toggle('show')
+//  })
+
+
+const Button = document.querySelector('.nav-toggel')
+const nav=document.querySelector('.header-menu')
+console.log(Button)
+console.log(nav)
+
+Button.addEventListener('click', ()=> {
+    nav.classList.toggle('show')
+ })
+
+
+
+ 
